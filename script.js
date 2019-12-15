654ca8f18f027f5b41cc698b517f1c9313b1bde3
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2019-12-15 21:54:05 +0100

    makes carasoul on mobile and begin flex

---
+++ script.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+//showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length - 1) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = n;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
